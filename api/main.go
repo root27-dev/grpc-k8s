363d67007f77dc2908f25e8e7f9deccc97989053
root27 <oguzhaneee@gmail.com>
2024-12-10 19:24:07 +0300

    update

--- api/main.go
+++ api/main.go
@@ -1,5 +1,53 @@
 package main
 
+import (
+	"context"
+	pb "github.com/root27-dev/grpc-k8s/pb"
+	"google.golang.org/grpc"
+	"log"
+	"net/http"
+	"strconv"
+)
+
 func main() {
+
+	conn, err := grpc.Dial("add-service:50051", grpc.WithInsecure())
+
+	if err != nil {
+
+		log.Fatalf("Failed to dial: %v", err)
+
+	}
+
+	addClient := pb.NewAddServiceClient(conn)
+
+	http.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request) {
+
+		queries := r.URL.Query()
+
+		if len(queries) == 0 {
+
+			w.Write([]byte("No queries found"))
+			return
+
+		}
+
+		a, _ := strconv.ParseUint(queries.Get("a"), 10, 32)
+		b, _ := strconv.ParseUint(queries.Get("b"), 10, 32)
+
+		req := &pb.AddRequest{A: a, B: b}
+
+		res, err := addClient.Add(context.Background(), req)
+
+		if err != nil {
+
+			w.Write([]byte("Failed to add: " + err.Error()))
+			return
+
+		}
+
+	})
+
+	http.ListenAndServe(":8080", nil)
 
 }
--- server/main.go
+++ server/main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"context"
+	pb "github.com/root27-dev/grpc-k8s/pb"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+type server struct {
+	pb.UnimplementedAddServiceServer
+}
+
 func main() {
 
+	lis, err := net.Listen("tcp", ":50051")
+
+	if err != nil {
+
+		log.Fatalf("Failed to listen: %v", err)
+
+	}
+
+	srv := grpc.NewServer()
+
+	pb.RegisterAddServiceServer(srv, &server{})
+
+	if err := srv.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+
+}
+
+func (s *server) Add(ctx context.Context, in *pb.AddRequest) (*pb.AddResponse, error) {
+	return &pb.AddResponse{Result: in.A + in.B}, nil
 }
